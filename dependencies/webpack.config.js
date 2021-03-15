339ce89a99843bcee0c17d2dec0ac54befb62787
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-03-15 22:21:12 +0100

    consume provided dependencies

--- dependencies/webpack.config.js
+++ dependencies/webpack.config.js
@@ -4,7 +4,7 @@ const settings = require('../webpack/settings');
 
 const { Config } = require('webpack-config');
 
-const manifest = require('./package.json');
+const manifest = require('./node_modules/@lastui/dependencies/package.json');
 
 module.exports = new Config()
 	.extend(path.resolve(settings.WEBPACK_ROOT_PATH, 'config/dll.js'))
