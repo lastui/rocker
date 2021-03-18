4325566fa2d05308e191e2275e6951712ab7a674
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-03-18 19:06:36 +0100

    wip

--- webpack/internal/development.js
+++ webpack/internal/development.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { WebpackPluginServe } = require('webpack-plugin-serve');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const settings = require(path.resolve(__dirname, "../settings"));
 
