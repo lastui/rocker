8b4ea06503c0061df06b6f8b62ee5b9e4c984c88
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-03-25 08:46:07 +0100

    github actions for automatic dependencies update

--- dependencies/webpack.config.js
+++ dependencies/webpack.config.js
@@ -2,8 +2,6 @@ const path = require("path");
 
 const settings = require("../webpack/settings");
 
-const manifest = require("./node_modules/@lastui/dependencies/package.json");
-
 const config = require(path.resolve(
 	settings.WEBPACK_ROOT_PATH,
 	"config/dll.js"
