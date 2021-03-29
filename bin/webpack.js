a304a46178be7d9d673fcf65d1884bd727c1a083
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-03-29 09:25:31 +0200

    test workflow

--- bin/webpack.js
+++ bin/webpack.js
@@ -1,49 +1,57 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const path = require('path');
-const { run } = require('webpack-nano/lib/compiler');
-const args = require('webpack-nano/argv');
+const chalk = require("chalk");
+const path = require("path");
+const { run } = require("webpack-nano/lib/compiler");
+const args = require("webpack-nano/argv");
 
-const end = () => process.exit(0);
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
 
-process.on('SIGINT', end);
-process.on('SIGTERM', end);
+if (process.env.NODE_ENV !== "production") {
+	process.env.NODE_ENV = "development";
+}
 
-if (process.env.NODE_ENV !== 'production') {
-	process.env.NODE_ENV = 'development';
-};
+function getConfig() {
+	if (args.config === undefined) {
+		return require(path.resolve("./webpack.config.js"));
+	}
+	return require(path.resolve(args.config));
+}
 
-const config = require(path.resolve(args.config === undefined
-	? './webpack.config.js'
-	: args.config));
-
-if (config.watch) {
+function patchEntrypoint(config) {
+	if (!config.watch) {
+		return config;
+	}
 	for (const entrypoint in config.entry) {
 		config.entry[entrypoint] = [
 			"webpack-plugin-serve/client",
 			...config.entry[entrypoint],
 		];
 	}
+	return config;
 }
 
 const logPrefix = {
-	ok: chalk.blue('⬡ webpack:'),
-	whoops: chalk.red('⬢ webpack:'),
+	ok: chalk.blue("⬡ webpack:"),
+	whoops: chalk.red("⬢ webpack:"),
 };
 
-const log = {
-	error: (...args) => {
-	  args.unshift(logPrefix.whoops);
-	  console.error(...args);
+const config = patchEntrypoint(getConfig());
+
+run(
+	{
+		config,
+		watchConfig: config.watch ? config : undefined,
 	},
-	info: (...args) => {
-	  args.unshift(logPrefix.ok);
-	  console.error(...args);
+	{
+		error: (...args) => {
+			args.unshift(logPrefix.whoops);
+			console.error(...args);
+		},
+		info: (...args) => {
+			args.unshift(logPrefix.ok);
+			console.error(...args);
+		},
 	}
-};
-
-run({
-	config,
-	watchConfig: config.watch ? config : undefined,
-}, log);
+);
