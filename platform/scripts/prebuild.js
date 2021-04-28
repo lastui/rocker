219f907055517ce0fa7b461b12f762186b86f02a
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-04-28 12:00:50 +0200

    fixing redux-devtools-extension linking

--- platform/scripts/prebuild.js
+++ platform/scripts/prebuild.js
@@ -1,10 +1,10 @@
 const { execSync } = require("child_process");
 const path = require("path");
-const cwd = path.resolve(process.cwd());
+const platform = path.resolve(process.cwd());
 
 execSync(`rm -rf ./node_modules`);
 execSync(`ln -s ../dependencies/node_modules ./node_modules`);
 
 execSync(`rm -rf ./node_modules/@lastui/rocker || :`);
 execSync(`mkdir -p ./node_modules/@lastui/rocker`);
-execSync(`ln -s ${cwd}/src ./node_modules/@lastui/rocker/platform`);
+execSync(`ln -s ${platform}/src ./node_modules/@lastui/rocker/platform`);
--- runtime/scripts/prebuild.js
+++ runtime/scripts/prebuild.js
@@ -1,12 +1,16 @@
 const { execSync } = require("child_process");
 const path = require("path");
-const cwd = path.resolve(__dirname, '..');
+const runtime = path.resolve(__dirname, '..');
 const platform = path.resolve(__dirname, '../../platform');
+const reduxDevtoolsExtension = path.resolve(__dirname, '../../node_modules/redux-devtools-extension');
 
 execSync(`rm -rf ./node_modules`);
 execSync(`ln -s ../dependencies/node_modules ./node_modules`);
 
 execSync(`rm -rf ./node_modules/@lastui/rocker || :`);
+execSync(`rm -rf ./node_modules/@lastui/redux-devtools-extension || :`);
 execSync(`mkdir -p ./node_modules/@lastui/rocker`);
+execSync(`mkdir -p ./node_modules/@lastui/redux`);
 execSync(`ln -s ${platform}/src ./node_modules/@lastui/rocker/platform`);
-execSync(`ln -s ${cwd}/src ./node_modules/@lastui/rocker/runtime`);
+execSync(`ln -s ${runtime}/src ./node_modules/@lastui/rocker/runtime`);
+execSync(`ln -s ${reduxDevtoolsExtension} ./node_modules/redux-devtools-extension`);
