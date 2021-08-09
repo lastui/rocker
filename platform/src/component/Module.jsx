d7918d7c9bc71152b1e7be3189a7389983a88f4a
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-08-10 00:47:54 +0200

    Modules to react to context update

--- platform/src/component/Module.jsx
+++ platform/src/component/Module.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { useModuleLoader } from "./ModuleContext";
 import ErrorBoundary from "./ErrorBoundary";
 
 const Module = (props) => {
   const moduleLoader = useModuleLoader();
-  const isLoaded = useSelector((state) =>
-    Boolean(state.shared.loaded[props.name])
-  );
   const buster = useSelector((state) => state.shared.buster);
 
   React.useEffect(() => {
