721d430ce498191be9265075ebebacd5caf7e1e1
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-08-12 01:31:03 +0200

    inductive routing path normalization

--- platform/src/component/Route.jsx
+++ platform/src/component/Route.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Route as ReactRoute } from "react-router";
 import { useRouteMatch } from "react-router-dom";
 
-const Route = () => {
+const Route = (props) => {
   const match = useRouteMatch();
   return React.createElement(ReactRoute, {
-    path: `${match.url}/${props.path}`,
+    path: `${match.url}/${props.path}`.replace(/\/+/g, '/'),
     exact: Boolean(props.exact),
     component: props.component,
   }, null);
