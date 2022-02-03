1b3646efac9f456e2656d7d2908ffcf6d93d88df
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-02-03 09:03:21 +0100

    split Route into Route and Branch

--- platform/src/component/Route.jsx
+++ platform/src/component/Route.jsx
@@ -3,52 +3,56 @@ import React from "react";
 import { RouterContext } from "./Router";
 import { matchPath } from "../routing";
 
+const Branch = (props) => {
+  const match = React.useMemo(
+    () => matchPath(
+      props.context.location.pathname,
+      `${props.context.match.url}/${props.path}`.replace(/\/+/g, "/"),
+      props.exact
+    ),
+    [props.path, props.exact, props.context.location.pathname, props.context.match.url]
+  );
+
+  const composite = React.useMemo(
+    () => {
+      if (match) {
+        return {
+          ...props.context,
+          match,
+        }
+      } else {
+        return props.context
+      }
+    },
+    [props.context, match]
+  );
+
+  if (!match) {
+    return null;
+  }
+
+  return (
+    <RouterContext.Provider value={composite}>
+      {React.createElement(props.component)}
+    </RouterContext.Provider>
+  );
+}
+
 const Route = (props) => {
-  if (!props.component) {
+  if (!props.component || !props.path) {
     return null;
   }
 
   return (
     <RouterContext.Consumer>
-      {(context) => {
-        const match = React.useMemo(
-          () => {
-            if (!props.path) {
-              return null
-            }
-            return matchPath(
-              context.location.pathname,
-              `${context.match.url}/${props.path}`.replace(/\/+/g, "/"),
-              props.exact
-            )
-          },
-          [props.path, props.exact, context.location.pathname, context.match.url]
-        );
-
-        const composite = React.useMemo(
-          () => {
-            if (match) {
-              return {
-                ...context,
-                match,
-              }
-            } else {
-              return context
-            }
-          },
-          [context, match]
-        );
-
-        if (!match) {
-          return null;
-        }
-
-        return (
-          <RouterContext.Provider value={composite}>
-            {React.createElement(props.component)}
-          </RouterContext.Provider>
-        );
-      }}
+      {(context) => (
+        <Branch
+          path={props.path}
+          exact={props.exact}
+          context={context}
+          component={props.component}
+        />
+      )}
     </RouterContext.Consumer>
   );
 };
