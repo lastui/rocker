4cc6985d3331d17e0e14168e912b0eb51ca74e69
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-02-07 13:37:04 +0000

    fix runtime async hook

--- runtime/src/component/Main.jsx
+++ runtime/src/component/Main.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { actions } from "@lastui/rocker/platform";
 import setupStore from "../store";
-import Entrypoint from './Entrypoint'
+import Entrypoint from "./Entrypoint";
 
 const Main = (props) => {
 	const [_, setErrorState] = React.useState();
 	const [store, setStore] = React.useState();
 
-	React.useEffect(async () => {
+	const bootstrap = React.useCallback(async () => {
+		console.debug("bootstraping runtime");
 		try {
 			const nextStore = await setupStore(props.reduxMiddlewares);
-			nextStore.dispatch(actions.init(props.fetchContext, props.initializeRuntime));
+			nextStore.dispatch(
+				actions.init(props.fetchContext, props.initializeRuntime)
+			);
 			setStore(nextStore);
 		} catch (error) {
 			setErrorState(() => {
 				throw error;
 			});
 		}
-	}, []);
+	}, [props.reduxMiddlewares, props.fetchContext, props.initializeRuntime]);
+
+	React.useEffect(() => {
+		bootstrap();
+	}, [bootstrap]);
 
 	if (!store) {
 		return null;
