4c904feece6793e3b5a7bef9ebf0fd6d2a0e804e
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-01 07:59:59 +0200

    fix sagas and middleware registerModule assertions

--- platform/src/register.js
+++ platform/src/register.js
@@ -1,47 +1,57 @@
-import { warning } from './utils';
+import { warning } from "./utils";
 
-export default function(scope) {
+export default function (scope) {
   if (scope === null || scope.constructor !== Object) {
-    throw new Error(`registerModule argument be object. Was: ${scope}`)
+    throw new Error(`registerModule argument must be plain object`);
   }
   if (scope.Main) {
-    if (scope.Main.constructor.name !== 'Function') {
-      warning('attribute Main provided in registerModule is not function');
+    if (scope.Main.constructor.name !== "Function") {
+      warning("attribute Main provided in registerModule is not function");
     } else {
       window.__SANDBOX_SCOPE__.Main = scope.Main;
     }
   }
   if (scope.Error) {
-    if (scope.Error.constructor.name !== 'Function') {
-      warning('attribute Error provided in registerModule is not function');
+    if (scope.Error.constructor.name !== "Function") {
+      warning("attribute Error provided in registerModule is not function");
     } else {
       window.__SANDBOX_SCOPE__.Error = scope.Error;
     }
   }
   if (scope.reducer) {
     if (scope.reducer.constructor !== Object) {
-      warning('attribute reducer provided in registerModule is not plain object');
+      warning("attribute reducer provided in registerModule is not plain object");
     } else {
       window.__SANDBOX_SCOPE__.reducer = scope.reducer;
     }
   }
   if (scope.middleware) {
-    if (!(middleware.constructor.name === 'Function' || middleware.constructor.name === 'AsyncFunction')) {
-      warning('attribute middleware provided in registerModule is not function');
+    if (
+      !(
+        scope.middleware.constructor.name === "Function" ||
+        scope.middleware.constructor.name === "AsyncFunction"
+      )
+    ) {
+      warning("attribute middleware provided in registerModule is not function or async function");
     } else {
       window.__SANDBOX_SCOPE__.middleware = scope.middleware;
     }
   }
   if (scope.saga) {
-    if (!(saga.constructor.name === 'GeneratorFunction' || saga.constructor.name === 'AsyncGeneratorFunction')) {
-      warning('attribute saga provided in registerModule is not generator function');
+    if (
+      !(
+        scope.saga.constructor.name === "GeneratorFunction" ||
+        scope.saga.constructor.name === "AsyncGeneratorFunction"
+      )
+    ) {
+      warning("attribute saga provided in registerModule is not generator function or async generator function");
     } else {
       window.__SANDBOX_SCOPE__.saga = scope.saga;
     }
   }
   if (scope.props) {
     if (scope.props.constructor !== Object) {
-      warning('attribute props provided in registerModule is not object');
+      warning("attribute props provided in registerModule is not object");
     } else {
       window.__SANDBOX_SCOPE__.props = scope.props;
     }
