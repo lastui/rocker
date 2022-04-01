f5f76e13fd053efc43891e9362015cdc6316af5b
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-01 05:55:39 +0000

    fix registerModule assertions

--- platform/src/utils.js
+++ platform/src/utils.js
@@ -1,8 +1,12 @@
 export function warning(message, throwable) {
-  if (typeof console !== 'undefined' && typeof console.error === 'function') {
-    console.error(message, throwable)
+  if (typeof console !== "undefined" && typeof console.error === "function") {
+    if (throwable) {
+      console.error(message, throwable);
+    } else {
+      console.error(message);
+    }
   }
   try {
-    throw new Error(message)
+    throw new Error(message);
   } catch (e) {}
 }
