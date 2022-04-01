98135d3fb54a2f729fba597164922a85b6df06b8
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-01 06:19:21 +0000

    provide better hints in registerModule assertions

--- platform/src/register.js
+++ platform/src/register.js
@@ -1,26 +1,39 @@
 import { warning } from "./utils";
 
+const GeneratorFunction = function* () {}.constructor;
+const AsyncGeneratorFunction = async function* () {}.constructor;
+const AsyncFunction = async function () {}.constructor;
+const PlainFunction = function () {}.constructor;
+
 export default function (scope) {
   if (scope === null || scope.constructor !== Object) {
-    throw new Error(`registerModule argument must be plain object`);
+    throw new Error(
+      `registerModule accepts only plain object, was called with ${scope.constructor}`
+    );
   }
   if (scope.Main) {
-    if (scope.Main.constructor.name !== "Function") {
-      warning("attribute Main provided in registerModule is not function");
+    if (!(scope.Main instanceof PlainFunction)) {
+      warning(
+        `attribute "Main" provided in registerModule is not function, it is ${scope.Main.constructor}`
+      );
     } else {
       window.__SANDBOX_SCOPE__.Main = scope.Main;
     }
   }
   if (scope.Error) {
-    if (scope.Error.constructor.name !== "Function") {
-      warning("attribute Error provided in registerModule is not function");
+    if (!(scope.Error instanceof PlainFunction)) {
+      warning(
+        `attribute "Error" provided in registerModule is not function, it is ${scope.Error.constructor}`
+      );
     } else {
       window.__SANDBOX_SCOPE__.Error = scope.Error;
     }
   }
   if (scope.reducer) {
     if (scope.reducer.constructor !== Object) {
-      warning("attribute reducer provided in registerModule is not plain object");
+      warning(
+        `attribute "reducer" provided in registerModule is not plain object, it is ${scope.reducer.constructor}`
+      );
     } else {
       window.__SANDBOX_SCOPE__.reducer = scope.reducer;
     }
@@ -28,11 +41,13 @@ export default function (scope) {
   if (scope.middleware) {
     if (
       !(
-        scope.middleware.constructor.name === "Function" ||
-        scope.middleware.constructor.name === "AsyncFunction"
+        scope.middleware instanceof PlainFunction ||
+        scope.middleware instanceof AsyncFunction
       )
     ) {
-      warning("attribute middleware provided in registerModule is not function or async function");
+      warning(
+        `attribute "middleware" provided in registerModule is not function or async function, it is ${scope.middleware.constructor}`
+      );
     } else {
       window.__SANDBOX_SCOPE__.middleware = scope.middleware;
     }
@@ -40,18 +55,22 @@ export default function (scope) {
   if (scope.saga) {
     if (
       !(
-        scope.saga.constructor.name === "GeneratorFunction" ||
-        scope.saga.constructor.name === "AsyncGeneratorFunction"
+        scope.saga instanceof GeneratorFunction ||
+        scope.saga instanceof AsyncGeneratorFunction
       )
     ) {
-      warning("attribute saga provided in registerModule is not generator function or async generator function");
+      warning(
+        `attribute "saga" provided in registerModule is not generator function or async generator function, it is ${scope.saga.constructor}`
+      );
     } else {
       window.__SANDBOX_SCOPE__.saga = scope.saga;
     }
   }
   if (scope.props) {
     if (scope.props.constructor !== Object) {
-      warning("attribute props provided in registerModule is not object");
+      warning(
+        `attribute "props" provided in registerModule is not object, it is ${scope.props.constructor}`
+      );
     } else {
       window.__SANDBOX_SCOPE__.props = scope.props;
     }
