834a6d32b51942a7c8dbd792d7b68137de540d6c
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-09 11:21:03 +0000

    jest --passWithNoTests

--- cli/helpers/jest.js
+++ cli/helpers/jest.js
@@ -5,6 +5,7 @@ const jest = require('jest');
 exports.run = async function() {
 	await jest.run([
 		'--colors',
+		'--passWithNoTests',
 		`--maxWorkers=${os.cpus().length}`,
 		'--config',
 		path.resolve(__dirname, '../../jest/index.js'),
