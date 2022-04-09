d1b6e25d88acd1540d8a7120cb751dbe34dd36ef
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-09 11:08:58 +0000

    patch redux devtools extention

--- bootstrap/scripts/prebuild.js
+++ bootstrap/scripts/prebuild.js
@@ -9,11 +9,11 @@ async function main() {
 	await clearDirectory(path.resolve(__dirname, "../node_modules"));
 	await createSymlink(path.resolve(__dirname, "../../dependencies/node_modules"), path.resolve(__dirname, "../node_modules"));
 	await clearDirectory(path.resolve(__dirname, "../node_modules/@lastui/rocker"));
-	await clearDirectory(path.resolve(__dirname, "../node_modules/redux-devtools-extension"));
+	await clearDirectory(path.resolve(__dirname, "../node_modules/@redux-devtools"));
 	await ensureDirectory(path.resolve(__dirname, "../node_modules/@lastui/rocker"));
 	await createSymlink(path.resolve(__dirname, "../../platform/src"), path.resolve(__dirname, "../node_modules/@lastui/rocker/platform"));
 	await createSymlink(path.resolve(__dirname, "../src"), path.resolve(__dirname, "../node_modules/@lastui/rocker/bootstrap"));
-	await createSymlink(path.resolve(__dirname, "../../node_modules/redux-devtools-extension"), path.resolve(__dirname, "../node_modules/redux-devtools-extension"));
+	await createSymlink(path.resolve(__dirname, "../../node_modules/@redux-devtools"), path.resolve(__dirname, "../node_modules/@redux-devtools"));
 }
 
 ////////////////////////////////
