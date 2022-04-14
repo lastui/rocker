af4e80b4f8234b4a999a2b979897a27b2a541a4a
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-14 16:39:28 +0000

    propagate all process.env variables to sources

--- webpack/internal/development.js
+++ webpack/internal/development.js
@@ -33,16 +33,32 @@ module.exports = {
 			Buffer: ["buffer", "Buffer"],
 			process: ["process"],
 		}),
-		new webpack.DefinePlugin({
-			process: {},
-			"process.env": {},
-			"process.env.NODE_ENV": `"development"`,
-			"process.env.NODE_DEBUG": false,
-			"process.env.VERSION":
-				process.env.VERSION !== undefined
-					? `"${process.env.VERSION}"`
-					: `""`,
-		}),
+		new webpack.DefinePlugin(
+			Object.entries(process.env).reduce(
+				(acc, [k, v]) => {
+					if (acc[k] === undefined) {
+						switch (typeof v) {
+							case "boolean":
+							case "number": {
+								acc[`process.env.${k}`] = v;
+								break;
+							}
+							default: {
+								acc[`process.env.${k}`] = `"${v}"`;
+								break;
+							}
+						}
+					}
+					return acc;
+				},
+				{
+					process: {},
+					"process.env": {},
+					"process.env.NODE_ENV": `"development"`,
+					"process.env.NODE_DEBUG": false,
+				}
+			)
+		),
 		new webpack.EnvironmentPlugin([
 			...Object.keys(process.env),
 			"NODE_ENV",
