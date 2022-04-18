464094c4b3e77736bd3dfa6d65e204d2705e7823
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-18 18:10:10 +0200

    fix testing after platform reorganisation

--- jest/__mocks__/platform.js
+++ jest/__mocks__/platform.js
@@ -1,17 +1,32 @@
 const React = require("react");
 
+const constants = new Proxy(Object, {
+	get(_ref, prop) {
+		return prop;
+	},
+});
+
 module.exports = {
-	Module: (props) => props.children
-		? React.createElement('section', { 'data-testid': `module/${props.name}` }, props.children)
-		: React.createElement('section', { 'data-testid': `module/${props.name}` }),
+	Module: (props) =>
+		props.children
+			? React.createElement(
+					"section",
+					{ "data-testid": `module/${props.name}` },
+					props.children
+			  )
+			: React.createElement("section", {
+					"data-testid": `module/${props.name}`,
+			  }),
 	Route: () => null,
-	Router: (props) => props.children
-		? React.createElement(React.Fragment, {}, props.children)
-		: null,
+	Router: (props) =>
+		props.children
+			? React.createElement(React.Fragment, {}, props.children)
+			: null,
 	Redirect: () => null,
-	Link: (props) => props.component
-		? React.createElement(props.component, { navigate: () => {} })
-		: null,
+	Link: (props) =>
+		props.component
+			? React.createElement(props.component, { navigate: () => {} })
+			: null,
 	useHistory: () => ({
 		push: () => {},
 		replace: () => {},
@@ -19,17 +34,22 @@ module.exports = {
 	useLocation: () => null,
 	useParams: () => null,
 	useRouteMatch: () => null,
-	constants: new Proxy(Object, {
-		get(_ref, prop) {
-			return prop;
-		},
-	}),
-	moduleLoaderMiddleware: (_moduleLoader) => (_store) => (next) => (action) => next(action),
-	dynamicMiddleware: (_store) => (next) => (action) => next(action),
-	moduleLoader: {
-		getModulesReducer: () => (state = {}, action) => state,
-		setSagaRunner: () => {},
-		setStore: () => {},
+	constants,
+	actions: {
+		setLanguage: (language) => ({
+			type: constants.SET_LANGUAGE,
+			payload: {
+				language,
+			},
+		}),
+		refresh: () => ({
+			type: constants.REFRESH,
+		}),
 	},
-	sharedState: (state = {}, action) => state,
+	moduleLoaderMiddleware: (_store) => (next) => (action) => next(action),
+	dynamicMiddleware: (_store) => (next) => (action) => next(action),
+	setSagaRunner: () => {},
+	setStore: () => {},
+	sharedReducer: (state = {}, action) => state,
+	modulesReducer: (state = {}, action) => state,
 };
