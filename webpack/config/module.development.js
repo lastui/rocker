aa916086c1862af282b41f87cb234eaef0279723
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-22 06:29:59 +0000

    improvements to babel - linaria plugin name isolation

--- webpack/config/module.development.js
+++ webpack/config/module.development.js
@@ -79,9 +79,17 @@ config.module.rules.push(
 					}),
 					plugins: babel.plugins.map((plugin) => {
 						if (!Array.isArray(plugin)) {
-							return [plugin, {}, `babel-${plugin}`];
+							return [
+								plugin,
+								{},
+								`babel-${plugin.name || plugin}`,
+							];
 						} else {
-							return [plugin[0], plugin[1], `babel-${plugin[2]}`];
+							return [
+								plugin[0],
+								plugin[1],
+								`babel-${plugin[2].name || plugin[2]}`,
+							];
 						}
 					}),
 					cacheDirectory: path.join(
@@ -122,12 +130,16 @@ config.module.rules.push(
 						}),
 						plugins: babel.plugins.map((plugin) => {
 							if (!Array.isArray(plugin)) {
-								return [plugin, {}, `linaria-${plugin}`];
+								return [
+									plugin,
+									{},
+									`linaria-${plugin.name || plugin}`,
+								];
 							} else {
 								return [
 									plugin[0],
 									plugin[1],
-									`linaria-${plugin[2]}`,
+									`linaria-${plugin[2].name || plugin[2]}`,
 								];
 							}
 						}),
