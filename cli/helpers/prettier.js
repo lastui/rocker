4cc2874c1ab4636cfc81dae7987123a88347b3c3
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-04-24 21:48:47 +0200

    remove eol in lint

--- cli/helpers/prettier.js
+++ cli/helpers/prettier.js
@@ -16,6 +16,6 @@ exports.run = async function () {
 		'--print-width=120',
 		'--trailing-comma=all',
 		'--write',
-		'(*\\..(js|ts|jsx|tsx)|(src/**/*\\.(js|ts|jsx|tsx)))',
+		'(*\\.(js|ts|jsx|tsx)|(src/**/*\\.(js|ts|jsx|tsx)))',
 	]);
 };
