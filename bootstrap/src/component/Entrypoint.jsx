ef1b646b232995e2464a6b5c2c3a925a9e6d878c
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-08-28 08:55:21 +0200

    switch to react-router v6 from custom router (parity of react-router was reached)

--- bootstrap/src/component/Entrypoint.jsx
+++ bootstrap/src/component/Entrypoint.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Module } from "@lastui/rocker/platform";
 import { getEntrypoint } from "../selector";
-import { createBrowserHistory } from "history";
 import Globalisation from "./Globalisation";
 
 import { BrowserRouter } from "react-router-dom";
