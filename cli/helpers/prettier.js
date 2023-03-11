08ff56a71338a7a34d4026c5088b4bfe4b884d49
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-11 08:06:10 +0100

    improve rocker lint command

--- cli/helpers/prettier.js
+++ cli/helpers/prettier.js
@@ -1,12 +1,11 @@
+const fs = require("fs");
+const path = require("path");
 const prettier = require("prettier/cli");
 
 exports.run = async function (options) {
   process.on("unhandledRejection", (reason) => {
     throw reason;
   });
-
-  const fs = require("fs");
-  const path = require("path");
 
   const cwd = options.cwd ? `${options.cwd.replaceAll(`.${path.delimiter}`, "")}${path.delimiter}` : "";
 
@@ -20,7 +19,7 @@ exports.run = async function (options) {
     "--end-of-line=lf",
     "--print-width=120",
     "--trailing-comma=all",
-    "--write",
+    ...(options.fix ? ["--write"] : []),
     fs.existsSync(path.resolve(process.env.INIT_CWD, "src"))
       ? `(${cwd}*\\.(js|ts|jsx|tsx)|(${cwd}${path.join("src", "**", "*")}\\.(js|ts|jsx|tsx)))`
       : `(${cwd}${path.join("**", "*")}\\.(js|ts|jsx|tsx))`,
