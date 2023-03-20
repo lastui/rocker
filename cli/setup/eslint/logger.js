db3df3af8e417095770a3c49c890d62013452973
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-20 18:27:15 +0100

    better eslint logging more inline with jest

--- cli/setup/eslint/logger.js
+++ cli/setup/eslint/logger.js
@@ -9,15 +9,15 @@ const processor = {
     return [text];
   },
   postprocess: ([messages = []] = [], filename) => {
-    const end = process.hrtime(durations[filename])
-    const duration = `${(end[0]*1_000 + end[1]/1_000_000).toFixed(2)}ms`;
+    const end = process.hrtime(durations[filename]);
+    const duration = `${(end[0] * 1_000 + end[1] / 1_000_000).toFixed(2)} ms`;
     const notice = path.relative(process.env.INIT_CWD, filename);
     if (messages.length === 0) {
-      console.log(colors.gray(`✔︎ ${notice}`), duration);
+      console.log(colors.green(`✓`), colors.dim(`${notice} (${duration})`));
     } else if (messages.some((item) => item.severity > 1)) {
-      console.log(colors.red(`✘ ${notice}`), duration);
+      console.log(colors.red(`✕`), colors.dim(`${notice} (${duration})`));
     } else {
-      console.log(colors.yellow(`▲ ${notice}`), duration);
+      console.log(colors.yellow(`!`), colors.dim(`${notice} (${duration})`));
     }
     return messages;
   },
