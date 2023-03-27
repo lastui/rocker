a4046b7d62ffeecc7004b0ba823fb880499526b5
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-27 19:14:31 +0200

    more stable DLL shims

---
+++ bootstrap/scripts/cleanup_volatile.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { readFile, writeFile } = require("../../cli/helpers/io");
+
+async function main() {
+  const manifestFile = path.resolve(__dirname, "..", "dll", "bootstrap-prod-manifest.json");
+  const manifest = JSON.parse(await readFile(manifestFile));
+
+  for (const key in manifest.content) {
+    if (key.startsWith("./node_modules/@babel/runtime")) {
+      delete manifest.content[key];
+    }
+  }
+
+  await writeFile(manifestFile, JSON.stringify(manifest, null, 2));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
