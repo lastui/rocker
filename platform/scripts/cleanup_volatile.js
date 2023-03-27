84edb9d6fa47e2b5c910e9314ae6db75a905a0e9
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-27 20:57:17 +0200

    more verbose cleanup volatile

--- platform/scripts/cleanup_volatile.js
+++ platform/scripts/cleanup_volatile.js
@@ -1,20 +1,32 @@
 const path = require("path");
 const { readFile, writeFile } = require("../../cli/helpers/io");
 
-async function main() {
-  const manifestFile = path.resolve(__dirname, "..", "dll", "platform-prod-manifest.json");
+async function cleanupManifest(filename) {
+  let changed = false;
+  console.log(`Cleaning up ${filename} file`);
+  const manifestFile = path.resolve(__dirname, "..", "dll", filename);
   const manifest = JSON.parse(await readFile(manifestFile));
 
   for (const key in manifest.content) {
     if (key.startsWith("./node_modules/@babel/runtime")) {
+      changed = true;
+      console.log(`dropping ${key} from ${filename}`);
       delete manifest.content[key];
     }
     if (key.startsWith("./node_modules/core-js")) {
+      changed = true;
+      console.log(`dropping ${key} from ${filename}`);
       delete manifest.content[key];
     }
   }
+  if (changed) {
+    console.log(`Patching ${filename} file`);
+    await writeFile(manifestFile, JSON.stringify(manifest, null, 2));
+  }
+}
 
-  await writeFile(manifestFile, JSON.stringify(manifest, null, 2));
+async function main(filename) {
+  await cleanupManifest("platform-prod-manifest.json");
 }
 
 main()
