9f7e7be8facb97efd4e0373e726cb0d22eac8f50
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-30 07:58:56 +0200

    cleanup setup

--- cli/commands/setup.js
+++
@@ -1,9 +0,0 @@
-exports.command = "setup";
-
-exports.describe = false;
-
-exports.builder = {};
-
-exports.handler = async function (options, cleanupHooks) {
-  await require("../setup/eslint")();
-};
