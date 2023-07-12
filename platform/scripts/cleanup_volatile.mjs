eafb7be02df9ae08d2991d8bfa2eebd6fbdf0dc7
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-07-12 07:06:03 +0200

    cleanup scripts

--- platform/scripts/cleanup_volatile.mjs
+++ platform/scripts/cleanup_volatile.mjs
@@ -8,20 +8,15 @@ async function cleanupManifest(filename) {
   const manifestFile = path.resolve(fileURLToPath(import.meta.url), "..", "..", "dll", filename);
   const manifest = JSON.parse(await fs.readFile(manifestFile, { encoding: "utf8" }));
 
+  const polyfills = [
+    "./node_modules/regenerator-runtime/",
+    "./node_modules/@babel/runtime",
+    "./node_modules/core-js",
+    "tslib/tslib.es6.js",
+  ];
+
   for (const key in manifest.content) {
-    if (key.startsWith("./node_modules/regenerator-runtime/")) {
-      changed = true;
-      delete manifest.content[key];
-    }
-    if (key.startsWith("./node_modules/@babel/runtime")) {
-      changed = true;
-      delete manifest.content[key];
-    }
-    if (key.startsWith("./node_modules/core-js")) {
-      changed = true;
-      delete manifest.content[key];
-    }
-    if (key.endsWith("tslib/tslib.es6.js")) {
+    if (polyfills.includes(key)) {
       changed = true;
       delete manifest.content[key];
     }
