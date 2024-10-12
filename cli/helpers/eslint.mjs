ef01d3373bec992118c8c184dde89081e16a87f4
Jan Cajthaml <jan.cajthaml@gmail.com>
2024-10-12 09:34:32 +0200

    fix eslint9

--- cli/helpers/eslint.mjs
+++ cli/helpers/eslint.mjs
@@ -1,85 +1,107 @@
 import process from "node:process";
+
+import globals from "globals";
+
 import eslint from "eslint";
 
 import babelConfig from "../../babel/index.js";
+import babelParser from "@babel/eslint-parser";
 
-export const config = {
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    sourceType: "module",
-    allowImportExportEverywhere: false,
-    ecmaFeatures: {
-      globalReturn: false,
-      jsx: true,
-    },
-    requireConfigFile: false,
-    babelOptions: babelConfig.env.development,
-  },
-  plugins: ["import", "react"],
-  rules: {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "no-debugger": "error",
-    "no-undef": "error",
-    "no-unused-vars": [
-      "error",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-        ignoreRestSiblings: true,
-      },
-    ],
-    "no-unused-expressions": "error",
-    eqeqeq: ["error", "always"],
-    "import/first": "error",
-    "import/consistent-type-specifier-style": ["warn", "prefer-top-level"],
-    "import/default": "warn",
-    "import/named": "warn",
-    "import/namespace": "warn",
-    "import/no-cycle": "error",
-    "import/no-duplicates": "error",
-    "import/no-amd": "error",
-    "import/no-mutable-exports": "error",
-    "import/no-named-as-default-member": "warn",
-    "import/no-useless-path-segments": "warn",
-    "import/no-self-import": "error",
-    "import/no-webpack-loader-syntax": "error",
-    "import/order": [
-      "warn",
-      {
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
+import pluginImport from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
+
+export const config = [
+  {
+    languageOptions: {
+      sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        ecmaFeatures: {
+          globalReturn: false,
         },
-        pathGroups: [
-          {
-            pattern: "@lastui/**",
-            group: "internal",
-            position: "before",
-          },
-        ],
-        pathGroupsExcludedImportTypes: ["@lastui/**"],
-        groups: [["builtin", "external"], "internal", "parent", ["sibling", "index"], ["object", "type"]],
-        distinctGroup: false,
+        requireConfigFile: false,
+        babelOptions: babelConfig.env.development,
       },
-    ],
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    linterOptions: {},
+    plugins: {
+      react: pluginReact,
+      import: pluginImport,
+    },
+    rules: {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "no-debugger": "error",
+      "no-undef": "error",
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "no-unused-expressions": "error",
+      eqeqeq: ["error", "always"],
+      "import/first": "error",
+      "import/consistent-type-specifier-style": ["warn", "prefer-top-level"],
+      "import/default": "warn",
+      "import/named": "warn",
+      "import/namespace": "warn",
+      "import/no-cycle": "error",
+      "import/no-duplicates": "error",
+      "import/no-amd": "error",
+      "import/no-mutable-exports": "error",
+      "import/no-named-as-default-member": "warn",
+      "import/no-useless-path-segments": "warn",
+      "import/no-self-import": "error",
+      "import/no-webpack-loader-syntax": "error",
+      "import/order": [
+        "warn",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: "@lastui/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["@lastui/**"],
+          groups: [["builtin", "external"], "internal", "parent", ["sibling", "index"], ["object", "type"]],
+          distinctGroup: false,
+        },
+      ],
+    },
   },
-};
+  {
+    files: ["**/*.jsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+];
 
 export async function createEngine(options) {
   const engine = new eslint.ESLint({
     cwd: process.env.INIT_CWD,
+    overrideConfigFile: true,
     allowInlineConfig: true,
-    useEslintrc: false,
     fix: options.fix,
     baseConfig: config,
   });
@@ -94,7 +116,7 @@ export async function createEngine(options) {
 
     try {
       start = process.hrtime();
-      const results = await engine.lintText(info.data, { filePath: info.filePath });
+      const results = await engine.lintText(info.data, { filePath: info.filePath, warnIgnored: true });
       end = process.hrtime(start);
       if (options.fix && results[0].output) {
         info.changed = true;
